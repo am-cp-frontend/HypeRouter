f5a88fafad216bd91b38867fd6420a47b81bc4ed
Vsevolod Trofimov <v-trof@yandex-team.ru>
2019-05-05 14:46:57 +0300

    Rename node to route to make clearer what switch child is

--- src/components/Router/Switch.tsx
+++ src/components/Router/Switch.tsx
@@ -8,18 +8,18 @@ import { matches } from './matchPath';
 export const Switch: React.FC = React.memo(({ children }) => {
     const location = React.useContext(locationCtx)
     let match: boolean = false
-    let node: React.ReactNode = null
+    let route: React.ReactNode = null
 
     React.Children.forEach(children, child => {
         if (!match && React.isValidElement(child)) {
             if (matches(child.props, location.path)) {
-                node = React.cloneElement(child)
+                route = React.cloneElement(child)
                 match = true
             }
         }
     })
 
-    return node
+    return route
 })
 
 Switch.displayName = 'Switch'
